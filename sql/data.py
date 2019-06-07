1ed3a836320fcbbc68c6c0704613b237083e6620
石榴 <txd1feng@163.com>
2019-06-07 20:49:02 +0800

    分

---
+++ sql/data.py
@@ -0,0 +1,20 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:mysql@127.0.0.1:3306/gblo'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
+    password_hash = db.Column(db.String(128))
+
+
+# generate_password_hash(password, method=pbkdf2:sha1, salt_length=8):
+    def __repr__(self):
+        return self.name
